77a756894306c847e26e765335759ca5ca9fb177
nesirid <nasirid@code.edu.az>
2024-03-28 03:47:57 +0400

    Async/Await in C#

---
+++ Thread-Files/Thread-Files/ExampleTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Files
+{
+    internal class ExampleTask
+    {
+        public async Task GetById()
+        {
+            var datas = await _context.Categories.where(mbox => mbox.id = id);
+            datas.name = "dwadawdwwa";
+        }
+        public async Task GetByName()
+        {
+            var datas = await _context.Categories.where(mbox => mbox.id = id);
+            datas.name = "dwadawdwwa";
+        }
+    }
+}
---
+++ Thread-Files/Thread-Files/Program.cs
@@ -0,0 +1,34 @@
+﻿void Method1()
+{
+    //Thread.Sleep(2000);
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine("Method1 - " + i);
+    }
+}
+void Method2()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine("Method2 - " + i);
+    }
+}
+
+void Method3()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine("Method3 - " + i);
+    }
+
+
+}
+
+Thread thread1 = new Thread(Method1);
+thread1.Start();
+
+Method3();
+
+Thread thread2 = new Thread(Method2);
+thread2.Start();
+
